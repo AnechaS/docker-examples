6e0ab782fb4ee3d333bb95bb2ca961aed2a2a7ae
Anecha Kuekharem <Anachitheu.k@gmail.com>
2023-05-12 18:18:29 +0000

    sample php connect mysql

--- php-mysql-phpmyadmin/src/app.php
+++ php-mysql-phpmyadmin/src/app.php
@@ -1,3 +1,15 @@
 <?php
 
-echo('Hello World!');
+try {
+    $db_host = "db";
+    $db_username = "root";
+    $db_password = "123456";
+    $db_name = "mysql";
+    $conn = new PDO("mysql:host=$db_host;dbname=$db_name", $db_username, $db_password);
+
+    $users = $conn->query("SELECT Host, User FROM `user`")->fetchAll(PDO::FETCH_ASSOC);
+
+    echo json_encode($users);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
